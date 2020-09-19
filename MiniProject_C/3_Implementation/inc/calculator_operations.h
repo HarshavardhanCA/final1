cad9b994e052aca948620da4540fa0e9038bd914
Harshavardhan C.A <69082733+stepin105005@users.noreply.github.com>
2020-09-19 09:45:40 +0530

    Update calculator_operations.h

--- MiniProject_C/3_Implementation/inc/calculator_operations.h
+++ MiniProject_C/3_Implementation/inc/calculator_operations.h
@@ -46,4 +46,17 @@ int divide(int operand1, int operand2);
 
 int module(int operand1, int operand2);
 
+int not(int operand1, int operand2);
+
+int and(int operand1, int operand2);
+
+int or(int operand1, int operand2);
+
+int nand(int operand1, int operand2);
+
+int nor(int operand1, int operand2);
+
+int xor(int operand1, int operand2);
+
+int xnor(int operand1, int operand2);
 #endif  /* #define __CALCULATOR_OPERATIONS_H__ */
