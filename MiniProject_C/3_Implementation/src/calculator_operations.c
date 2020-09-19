e8a0c19ba1de6de06920985b0aaefcc6dea2a82b
Harshavardhan C.A <69082733+stepin105005@users.noreply.github.com>
2020-09-19 09:43:48 +0530

    Update calculator_operations.c

--- MiniProject_C/3_Implementation/src/calculator_operations.c
+++ MiniProject_C/3_Implementation/src/calculator_operations.c
@@ -22,7 +22,43 @@ int divide(int operand1, int operand2)
     else
         return operand1 / operand2;
 }
+
 int module(int operand1, int operand2)
 {
     return operand1 % operand2;
+}
+
+int not(int operand1)
+{
+    return !(operand1);
+}
+
+int and(int operand1, int operand2)
+{
+    return operand1 && operand2;
+}
+
+int or(int operand1, int operand2)
+{
+    return operand1 || operand2;
+}
+
+int nand(int operand1, int operand2)
+{
+    return !(operand1 && operand2);
+}
+
+int nor(int operand1, int operand2)
+{
+    return !(operand1 || operand2);
+}
+
+int xor(int operand1, int operand2)
+{
+    return (operand1 ^ operand2);
+}
+
+int xnor(int operand1, int operand2)
+{
+    return !(operand1 ^ operand2);
 }
